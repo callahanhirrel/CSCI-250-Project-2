d74d2d00e0757e62cf39fe01ed9b5a1664faee76
spainhour <msspainhour@gmail.com>
2017-02-28 21:58:15 -0600

    deleted unnecessary lines

--- src/everythingElse/LoginGuiController.java
+++ src/everythingElse/LoginGuiController.java
@@ -1,13 +1,11 @@
 package everythingElse;
 
 import javafx.fxml.FXML;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
