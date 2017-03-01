51036b00b790e237bd5b1f203d8e877fb3fb5acd
callahanhirrel <callahan.hirrel@gmail.com>
2017-03-01 10:44:19 -0600

    last minute freakout

--- src/everythingElse/ProjectGuiController.java
+++ src/everythingElse/ProjectGuiController.java
@@ -1,8 +1,12 @@
 package everythingElse;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+//import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,49 +20,56 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 public class ProjectGuiController {
-	private ArrayList<String> arrays = new ArrayList<>();
+	//public List<File> arrays = new ArrayList<>();
 	//private ArrayList<String> transfer = new ArrayList<>();
-
+	
 	public void initialize() {
-
+		
 	}
-
+	
 	public void show_file(String filename) {
 		Label label = new Label(filename);
 		fileContainer.getChildren().add(label);
 	}
-
+	
 	@FXML
 	VBox fileContainer;
-
+	
 	@FXML
 	Button addFile;
-
+	
 	@FXML
 	Button rmFile;
-
+	
 	@FXML
 	ScrollPane hasAddedFile;
-
+	
 	@FXML
 	void add_file() {
+		addFile.getScene().getWindow().hide();
 		try{
+			/*
 			ArrayList<String> trans = new ArrayList<>();
 			//addFile.setText("Test");
 			for (int j = 0; j < arrays.size(); j++) {
 				trans.add(arrays.get(j));
 			}
-
+			*/
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(ProjectGuiMain.class.getResource("Project_GUI.fxml"));
 			AnchorPane root = (AnchorPane) loader.load();
-
-			ProjectGuiController pgc = (ProjectGuiController)loader.getController();
-
+			
+			everythingElse.ProjectGuiController pgc = (everythingElse.ProjectGuiController)loader.getController();
+			/*
+			for (File files : arrays) {
+				pgc.arrays.add(files);
+			}
+			*/
 			Stage stage = new Stage();
 			Scene scene = new Scene(root, 600, 371);
 			stage.setScene(scene);
 			stage.show();
+			
 			FileChooser fileChooser = new FileChooser();
 			fileChooser.setTitle(addFile.getText());
 			fileChooser.getExtensionFilters().addAll(
@@ -66,25 +77,48 @@ public class ProjectGuiController {
 					//new ExtensionFilter("MP3", "*.mp3"),
 					//new ExtensionFilter("WAV", "*.wav")
 					);
-
-			List<File> list = fileChooser.showOpenMultipleDialog(stage);
-
-			for (int k = 0; k < trans.size(); k++) {
-				arrays.add(trans.get(k));
+			
+			//ArrayList<String> trans2 = new ArrayList<>();
+			//addFile.setText("Test");
+			/*
+			for (int x = 0; x < trans.size(); x++) {
+				trans2.add(trans.get(x));
 			}
-
+			*/
+			List<File> list = fileChooser.showOpenMultipleDialog(stage);
+			
 			if (list != null) {
 				//add.add(file.getName());
-
+				/*
+				for (int k = 0; k < trans2.size(); k++) {
+					arrays.add(trans2.get(k));
+				}
+				*/
+				File f = new File("store_message.txt");
+				PrintWriter printer = new PrintWriter(new FileWriter(f, true));
+				
 				for (File file : list) {
-					arrays.add(file.getName());
+					printer.println(file.getName());
+					
 				}
-
-				for (int i = 0; i < arrays.size(); i++) {
-					pgc.show_file(arrays.get(i));
+				
+				printer.close();
+				
+				try {
+					Scanner input = new Scanner(f);
+					//System.out.print(f.getName());
+					while (input.hasNextLine()) { 
+						String line = input.nextLine();
+						//System.out.print(line);
+						pgc.show_file(line);
+					}
+				
+					input.close();
+					
+				} catch (Exception exc) {
+					exc.printStackTrace();
 				}
-			}
-			addFile.getScene().getWindow().hide();
+			}					
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
