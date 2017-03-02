67bd830197da0a012c582996d24c78585e433c71
callahanhirrel <callahan.hirrel@gmail.com>
2017-03-01 20:49:52 -0600

    some moar stuff

--- src/everythingElse/LoginGuiMain.java
+++ src/everythingElse/LoginGuiMain.java
@@ -1,4 +1,6 @@
 package everythingElse;
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -18,8 +20,10 @@ public class LoginGuiMain extends Application {
 		primaryStage.show();
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		launch(args);
+		Server s = new Server(8881); // gonna use 8881 as the port for now
+		s.listen();
 	}
 	
 }
