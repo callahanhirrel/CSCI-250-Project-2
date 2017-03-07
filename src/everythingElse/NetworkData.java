1cb5ff6e38b9250c565150afa227d52a91218e31
callahanhirrel <callahan.hirrel@gmail.com>
2017-03-06 22:02:28 -0600

    sum changes

--- src/everythingElse/NetworkData.java
+++ src/everythingElse/NetworkData.java
@@ -1,5 +1,41 @@
 package everythingElse;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
 public class NetworkData {
+
+	private String tag; // instruction for the server/client
+	private String msg; // data being sent
+	private byte[] file; // byte array of sound file
+
+	public NetworkData(String tag, String data) {
+		this.tag = tag;
+		placeData(data);
+	}
+
+	public NetworkData(String packet) {
+
+	}
+
+	private void placeData(String data) {
+		if (tag.equals("USERNAME")) {
+			this.msg = data;
+		} else if (tag.equals("FILE")) {
+			convertToBytes(data);
+		}
+	}
+
+	private void convertToBytes(String data) {
+		Path path = (Path) Paths.get(System.getProperty("user.dir"), "/new_folder/" + data);
+		try {
+			this.file = Files.readAllBytes(path);
+		} catch (IOException e) {
+			// TODO run an alert
+			e.printStackTrace();
+		}
+	}
 
 }
