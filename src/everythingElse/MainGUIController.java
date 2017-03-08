41ffc257cabee282955dcd74097dafa13a117005
callahanhirrel <callahan.hirrel@gmail.com>
2017-03-07 19:52:10 -0600

    changes

--- src/everythingElse/MainGUIController.java
+++ src/everythingElse/MainGUIController.java
@@ -84,11 +84,7 @@ public class MainGUIController {
 			createAlert("Invalid Project Name");
 		}
 	}
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> origin/master
 	private void createAlert(String message) {
 		Alert alert = new Alert(AlertType.ERROR, message, ButtonType.OK);
 		alert.showAndWait();
